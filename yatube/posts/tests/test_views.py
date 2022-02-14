463ca8686026b4f01a6f78aa81d71880b78ee300
Савва Романов <savva.romanov99@gmail.com>
2022-02-14 16:00:14 +0300

    v

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -33,13 +33,17 @@ class TaskPagesTests(TestCase):
         templates_pages_names = {
             reverse('posts:index'): 'posts/index.html',
             reverse('posts:group',
-                    kwargs={'slug': self.group.slug}): 'posts/group_list.html',
+                    kwargs={'slug': self.group.slug}):
+                'posts/group_list.html',
             reverse('posts:profile',
-                    kwargs={'username': self.user.username}): 'posts/profile.html',
+                    kwargs={'username': self.user.username}):
+                'posts/profile.html',
             reverse('posts:post_detail',
-                    kwargs={'post_id': self.post.pk}): 'posts/post_detail.html',
+                    kwargs={'post_id': self.post.pk}):
+                'posts/post_detail.html',
             reverse('posts:post_edit',
-                    kwargs={'post_id': self.post.pk}): 'posts/create_post.html',
+                    kwargs={'post_id': self.post.pk}):
+                'posts/create_post.html',
             reverse('posts:create'): 'posts/create_post.html'
         }
         for reverse_name, template in templates_pages_names.items():
