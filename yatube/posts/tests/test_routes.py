c67bd7bbbfa0168a6766109b3be778e50f9317bf
Савва Романов <savva.romanov99@gmail.com>
2022-02-26 16:35:09 +0300

    pomogite

--- yatube/posts/tests/test_routes.py
+++ yatube/posts/tests/test_routes.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
 
-
 SLUG = 'testgroup'
 USERNAME = 'TestAuthor'
 POST_ID = 1
@@ -9,31 +8,31 @@ POST_ID = 1
 
 class ReverseTests(TestCase):
     urls_names = [
-    [
-                '/',
-                reverse('posts:index')
-            ],
-            [
-                '/create/',
-                reverse('posts:create')
-            ],
-            [
-                f'/group/{SLUG}/',
-                reverse('posts:group', args=[SLUG])
-            ],
-            [
-                f'/profile/{USERNAME}/',
-                reverse('posts:profile', args=[USERNAME])
-            ],
-            [
-                f'/posts/{POST_ID}/',
-                reverse('posts:post_detail', args=[POST_ID])
-            ],
-            [
-                f'/posts/{POST_ID}/edit/',
-                reverse('posts:post_edit',
-                        args=[POST_ID])
-            ]
+        [
+            '/',
+            reverse('posts:index')
+        ],
+        [
+            '/create/',
+            reverse('posts:create')
+        ],
+        [
+            f'/group/{SLUG}/',
+            reverse('posts:group', args=[SLUG])
+        ],
+        [
+            f'/profile/{USERNAME}/',
+            reverse('posts:profile', args=[USERNAME])
+        ],
+        [
+            f'/posts/{POST_ID}/',
+            reverse('posts:post_detail', args=[POST_ID])
+        ],
+        [
+            f'/posts/{POST_ID}/edit/',
+            reverse('posts:post_edit',
+                    args=[POST_ID])
+        ]
     ]
 
     def test_url_uses_correct_reverse(self):
